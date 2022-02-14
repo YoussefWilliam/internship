0ca41f0f6a99d851fbb6231f1cd866ecf5cb7f03
YoussefWilliam <youssef.william997@gmail.com>
2022-02-14 13:14:40 +0200

    separate function

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <h1> hey there, i am count {count}</h1>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={increment}>
         <h1>click to increment</h1>
       </button>
     </div>

c9b349c0a8e380838eb4676dfbeae614a8af387e
YoussefWilliam <youssef.william997@gmail.com>
2022-02-14 13:09:11 +0200

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(count + 1);
+  }, []);
 
   return (
     <div className="App">
